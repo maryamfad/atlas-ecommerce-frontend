eb977fb98881a203d2a4bf22cb505b81fbb4f77e
Maryam Fadaee <maryamfadaee@gmail.com>
2024-09-05 14:10:25 -0400

    create sign up component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { SignupComponent } from './features/auth/signup/signup.component';
+export const routes: Routes = [
+    {
+        path: 'signup', 
+        component: SignupComponent 
+      },
+      {
+        path: '', 
+        redirectTo: '/signup', 
+        pathMatch: 'full'
+      },
+      {
+        path: '**', // Wildcard route for a 404 page (optional)
+        redirectTo: '/signup'
+      }
+];
---
+++ src/app/features/auth/signup/signup.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+})
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group(
+      {
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validator: this.mustMatch('password', 'confirmPassword'),
+      }
+    );
+  }
+
+  mustMatch(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.controls[password];
+      const confirmPasswordControl = formGroup.controls[confirmPassword];
+
+      if (
+        confirmPasswordControl.errors &&
+        !confirmPasswordControl.errors['mustMatch']
+      ) {
+        return;
+      }
+
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({ mustMatch: true });
+      } else {
+        confirmPasswordControl.setErrors(null);
+      }
+    };
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.signupForm.invalid) {
+      return;
+    }
+
+    console.log(this.signupForm.value);
+  }
+
+  get f() {
+    return this.signupForm.controls;
+  }
+}
